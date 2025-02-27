00fef6e98ce79f25bf16ba26c8d9db8098e475ab
Luis-Ortiz <20221810@aloe.ulima.edu.pe>
2025-02-26 20:48:25 -0500

    ugughg

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Proyecto-PW-2025_0/"
+  base: "/ProyectoGHPages/"
 })
