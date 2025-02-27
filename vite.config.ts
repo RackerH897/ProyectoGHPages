9d5144342b1a5510f0efcaa445b528876e158bba
Luis-Ortiz <20221810@aloe.ulima.edu.pe>
2025-02-26 20:43:45 -0500

    Kevin

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/ProyectoGHPages/"
+  base: "/Proyecto-PW-2025_0/"
 })
